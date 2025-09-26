45d48555e5fc73465ec979d4fb911315bbddf1c2
Zorkan Azhret <damien.carrere@protonmail.ch>
2025-09-26 11:45:50 +0200

    add visiteur

---
+++ visiteurs.php
@@ -0,0 +1,15 @@
+<?php
+class Visiteur extends Zoo
+{
+    protected string $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+}
